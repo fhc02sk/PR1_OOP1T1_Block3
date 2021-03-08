75acdc68c2762a0c965adbff63faeab5fb4af46a
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-08 15:00:19 +0100

    Array-Examples

---
+++ src/ExamplesWithArray.java
@@ -0,0 +1,22 @@
+public class ExamplesWithArray {
+    public static void main(String[] args) {
+
+
+        int[] arr = {
+                98, 97, 87, 86, 3, 81, 77, 74, 72, 69, 68, 65, 61,
+                51, 52, 59, 54, 47, 111, 42, 38, 51, 37, 29, 24, 81,
+                12, 29, 2, 17, 8   };
+
+        System.out.println("arr.length = " + arr.length);
+
+    }
+
+    // TODO 1: Write a method, finding and returning the smallest number
+    // TODO 2: Write a method, finding and returning the biggest number
+    // TODO 3: Write a method, calculating and returning the sum of all numbers with a while loop
+    // TODO 4: Write a method, calculating and returning the average over all numbers
+    // TODO 5 (complicated): Write a method, which returns an array with duplicated numbers (81, 51)
+
+
+
+}
