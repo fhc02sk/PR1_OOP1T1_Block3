57f74000146c984d33a5ad32d05382d3a8290399
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-08 11:09:06 +0100

    AccountCalculation

--- src/ArithmeticTowerDemo.java
+++ src/ArithmeticTowerDemo.java
@@ -1,5 +1,18 @@
 public class ArithmeticTowerDemo {
     public static void main(String[] args) {
 
+        long startValue = 202103;
+
+        System.out.println(startValue);
+        for (int i = 2; i <= 9; i++) {
+            System.out.println(startValue + " * " + i);
+            startValue = startValue * i;
+        }
+        for (int i = 2; i <= 9; i++) {
+            System.out.println(startValue + " / " + i);
+            startValue = startValue / i;
+        }
+
+        System.out.println(startValue);
     }
 }
